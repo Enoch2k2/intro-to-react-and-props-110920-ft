09c5613516df6ee720e81df79817c4614818bd4e
EliasAHH <eliasiscoding@gmail.com>
2021-03-24 09:38:11 -0700

    react hooks done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,100 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Form from './components/Form';
 import List from './components/List';
 
-class App extends Component {
-  state = {
-    blogs: []
+
+
+
+const App = () => {
+ // Use State ALWAYS returns an ARRAY with 2 elements 
+  const [blogs, setBlogs ] = useState([])
+  // const [id, setId] = useState(1)
+
+
+  useEffect(() => {
+    console.log("HI there you're in useEffect")
+    getAllPost()
+    // fetch(`http://localhost:3001/posts/${id}`)
+    //       .then(r => r.json())
+    //       .then(data => {
+    //         setBlogs(data)
+    //       })
+
+    // WHen you return a function useEffect will treat this as componentWillUnmount
+  },[])
+
+
+
+// const changeId = () => {
+//   setId(id + 1 )
+// }
+
+
+  const getAllPost = () => {
+        fetch("http://localhost:3001/posts")
+        .then(r => r.json())
+        .then(data => {
+          setBlogs(data)
+        })
+      }
+
+
+
+
+
+  const addBlog = blog => {
+    setBlogs( [...blogs, blog])
   }
+ 
 
-
-  // "DOM COntent Loaded kinda feel"
-
-  // Lifecycle methods ONLY WORK in CLass components. 
-
-  // spelling matters
-
-  componentDidMount() {
-    this.getAllPost()
-  }
-
-  getAllPost = () => {
-    fetch("http://localhost:3001/posts")
-    .then(r => r.json())
-    .then(data => {
-       this.setState({blogs: data})
-    })
-  }
-
-  addBlog = blog => {
-    this.setState({
-      blogs: [...this.state.blogs, blog]
-    })
-  }
-
-  render() {
-    return (
+  return (
         <div className="App">
-            <List blogs={ this.state.blogs } />
-            <Form  addBlog={ this.addBlog } />
+            <List blogs={ blogs } />
+            <Form  addBlog={ addBlog } />
         </div>
     );
-  }
+
 }
+
+// class App extends Component {
+//   state = {
+//     blogs: []
+//   }
+
+
+//   // "DOM COntent Loaded kinda feel"
+
+//   // Lifecycle methods ONLY WORK in CLass components. 
+
+//   // spelling matters
+
+//   componentDidMount() {
+//     this.getAllPost()
+//   }
+
+//   getAllPost = () => {
+//     fetch("http://localhost:3001/posts")
+//     .then(r => r.json())
+//     .then(data => {
+//        this.setState({blogs: data})
+//     })
+//   }
+
+//   addBlog = blog => {
+//     this.setState({
+//       blogs: [...this.state.blogs, blog]
+//     })
+//   }
+
+//   render() {
+//     return (
+//         <div className="App">
+//             <List blogs={ this.state.blogs } />
+//             <Form  addBlog={ this.addBlog } />
+//         </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,73 +1,134 @@
-import React, { Component } from 'react';
-
-class Form extends Component {
-
-  state = {
-    title: "",
-    content: ""
-  }
+import React, { useState, useEffect } from 'react';
 
 
- // this particular function allows us for a more control flow state 
 
- // THIS MUST RETURN A BOOLEAN 
-  shouldComponentUpdate(nextProps, nextState) {
-     console.log(nextProps, "this is the next props");
-     console.log(nextState, "this is the nextState");
-     if(this.state.title !== nextState.title) {
-       return true
-     } else {
-       return false
-     }
-  }
+const Form =  (props) => {
+  const [title, setTitle] =  useState("")
+  const [content, setContent] =  useState("")
 
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("Updated");
-    console.log(prevProps, "this was the previous Props");
-    console.log(prevState, "this was the previous State")
-  }
+  useEffect(() => {
+    console.log("I've rerendered")
+  },[title])
+
+    // const  [values, setValues] =  useState({
+    //   title: "",
+    //   content: ""
+    // })
 
 
-  handleChange = e => {
-    this.setState( prevState => {
-      return {
-        [e.target.name]: e.target.value
-      }
-    })
-  }
+    // const handleChange = e => {
+    //   setValues({...values, [e.target.name]: e.target.value})
+    //   }
 
-  handleSubmit = e => {
-    e.preventDefault();
 
-    this.props.addBlog(this.state)
+    const handleSubmit = e => {
+          e.preventDefault();
+          let  values = {
+            title: title, 
+            content: content
+          }
 
-    this.setState({
-      title: "",
-      content: ""
-    })
+          props.addBlog(values)
+
+        //  setValues({
+        //    title: "",
+        //    content: ""
+        //  })
+    }
+
+
+
+
+     return (
+          <form onSubmit={ handleSubmit }>
+            <h2>Create Blog</h2>
+            <div>
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" name="title" value={title} onChange={ e => setTitle(e.target.value) }  />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="content">Content</label>
+              <textarea id="content" name="content" value={ content } onChange={ e => setContent(e.target.value) }></textarea>
+            </div>
+            <br />
+            <input type="submit" value="Create Blog" />
+          </form>
+        )
+
+
+
+}
+
+// class Form extends Component {
+
+//   state = {
+//     title: "",
+//     content: ""
+//   }
+
+
+//  // this particular function allows us for a more control flow state 
+
+//  // THIS MUST RETURN A BOOLEAN 
+//   // shouldComponentUpdate(nextProps, nextState) {
+//   //    console.log(nextProps, "this is the next props");
+//   //    console.log(nextState, "this is the nextState");
+//   //    if(this.state.title !== nextState.title) {
+//   //      return true
+//   //    } else {
+//   //      return false
+//   //    }
+//   // }
+
+
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("Updated");
+//     console.log(prevProps, "this was the previous Props");
+//     console.log(prevState, "this was the previous State")
+//   }
+
+
+//   handleChange = e => {
+//     this.setState( prevState => {
+//       return {
+//         [e.target.name]: e.target.value
+//       }
+//     })
+//   }
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.addBlog(this.state)
+
+//     this.setState({
+//       title: "",
+//       content: ""
+//     })
 
     
-  }
+//   }
 
-  render() {
-    return (
-      <form onSubmit={ this.handleSubmit }>
-        <h2>Create Blog</h2>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" value={this.state.title} onChange={ this.handleChange }  />
-        </div>
-        <br />
-        <div>
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" value={ this.state.content } onChange={ this.handleChange }></textarea>
-        </div>
-        <br />
-        <input type="submit" value="Create Blog" />
-      </form>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <form onSubmit={ this.handleSubmit }>
+//         <h2>Create Blog</h2>
+//         <div>
+//           <label htmlFor="title">Title</label>
+//           <input type="text" id="title" name="title" value={this.state.title} onChange={ this.handleChange }  />
+//         </div>
+//         <br />
+//         <div>
+//           <label htmlFor="content">Content</label>
+//           <textarea id="content" name="content" value={ this.state.content } onChange={ this.handleChange }></textarea>
+//         </div>
+//         <br />
+//         <input type="submit" value="Create Blog" />
+//       </form>
+//     )
+//   }
+// }
 
 export default Form;
